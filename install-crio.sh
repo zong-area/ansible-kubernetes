d785061eb384ca8f21271e462c3a2cc2733ee102
zong-area <zongyu16321@gmail.com>
2024-05-21 08:19:45 +0800

    add crio cni plugin

--- install-crio.sh
+++ install-crio.sh
@@ -2,5 +2,7 @@
 yum install epel-release -y
 yum install -y   containers-common   device-mapper-devel   git   glib2-devel   glibc-static   go  libassuan-devel   libgpg-error-devel   libseccomp-devel   libselinux-devel   pkgconf-pkg-config  make   runc  --enablerepo=crb
 yum install -y gpgme-devel --enablerepo=crb
-git clone https://github.com/cri-o/cri-o   /home/kiosk/download/cri-o
-cd /home/kiosk/download/cri-o/ ; make ; sudo make install 
+git clone https://github.com/cri-o/cri-o   /opt/cri-o
+cd /opt/cri-o/ ; make ; sudo make install 
+sudo swapoff -a
+sysctl net.ipv4.ip_forward=1
